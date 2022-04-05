a18d255e2fff0b100cf1a38f29814ae87d9da705
justin-hill426 <jrhill426@gmail.com>
2022-04-05 14:04:52 -0600

    Added menuitem functionality

--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -26,6 +26,7 @@ const Header = () => {
   const [value, setValue] = useState(0);
   const [anchorEl, setAnchorEl] = useState(null)
   const [open, setOpen] = useState(false)
+  const [selectedIndex, setSelectedIndex] = useState(0)
 
   const handleChange = (e, value) => {
     setValue(value)
@@ -41,19 +42,74 @@ const Header = () => {
     setOpen(false)
   }
 
+  const handleMenuItemClick = (e, i) => {
+    setAnchorEl(null);
+    setOpen(false)
+    setSelectedIndex(i);
+    console.log(`Selected index is now ${i}`)
+    console.log(`Value index is now ${value}`)
+  }
+
+  const menuOptions = [
+    {name: 'Services', link: '/services'},
+    {name: 'Custom Software Development', link: '/customsoftware'},
+    {name: 'Mobile App Develpment', link: '/mobileapps'},
+    {name: 'Website Development', link: '/websites'},
+  ]
+
   useEffect(() => {
-    if(window.location.pathname === "/" && value !== 0) {
-      setValue(0)
-    } else if(window.location.pathname === "/services" && value !== 1) {
-      setValue(1)
-    } else if(window.location.pathname === "/revolution" && value !== 2) {
-      setValue(2)
-    } else if(window.location.pathname === "/about" && value !== 3) {
-      setValue(3)
-    } else if(window.location.pathname === "/contact" && value !== 4) {
-      setValue(4)
-    } else if(window.location.pathname === "/estimate" && value !== 5) {
-      setValue(5)
+    switch(window.location.pathname) {
+      case "/":
+        if(value !== 0) {
+          setValue(0);
+        }
+        break;
+      case "/services":
+        if(value !== 1) {
+          setValue(1);
+          setSelectedIndex(0);
+        }
+        break;
+      case "/customsoftware":
+        if(value !== 1) {
+          setValue(1);
+          setSelectedIndex(1);
+        }
+        break;
+      case "/mobileapps":
+        if(value !== 1) {
+          setValue(1);
+          setSelectedIndex(2);
+        }
+        break;
+      case "/websites":
+        if(value !== 1) {
+          setValue(1);
+          setSelectedIndex(3);
+        }
+        break;
+        case "/revolution":
+          if(value !== 2) {
+            setValue(2);
+          }
+          break;
+        case "/about":
+          if(value !== 3) {
+            setValue(3);
+          }
+          break;
+        case "/contact":
+          if(value !== 4) {
+            setValue(4);
+          }
+          break;
+        case "/estimate":
+          if(value !== 5) {
+            setValue(5);
+          }
+          break;
+      default:
+        break;
     }
   }, [value]);
 
@@ -175,50 +231,31 @@ const Header = () => {
                   color: 'white'
                 }
               })}
-              MenuListProps={{onMouseLeave: handleClose}}
+              anchorOrigin={{
+                vertical: 'top',
+                
+              }}
+              MenuListProps={{
+                onMouseLeave: handleClose,
+              }}
             >
-              <MenuItem 
-                onClick={() => {handleClose(); setValue(1)}} 
-                component={Link} 
-                sx={(theme) => ({
-                  ...theme.typography.tab,
-                  opacity: 0.7,
-                  "&:hover": {
-                    opacity: 1
-                  }
-                })}
-                to="/customsoftware"
-              >
-                Custom Software Development
-              </MenuItem>
-              <MenuItem 
-                onClick={() => {handleClose(); setValue(1)}} 
-                component={Link} 
-                sx={(theme) => ({
-                  ...theme.typography.tab,
-                  opacity: 0.7,
-                  "&:hover": {
-                    opacity: 1
-                  }
-                })}
-                to="/mobileapps"
-              >
-                Mobile App Development
-              </MenuItem>
-              <MenuItem 
-                onClick={() => {handleClose(); setValue(1)}} 
-                component={Link} 
-                sx={(theme) => ({
-                  ...theme.typography.tab,
-                  opacity: 0.7,
-                  "&:hover": {
-                    opacity: 1
-                  }
-                })}
-                to="/websitedevelopment"
-              >
-                Website Development
-              </MenuItem>
+              {menuOptions.map((option, i) => 
+                (<MenuItem 
+                  onClick={(event) => {handleClose(); setValue(1); handleMenuItemClick(event, i);}} 
+                  component={Link} 
+                  key={i}
+                  sx={(theme) => ({
+                    ...theme.typography.tab,
+                    opacity: 0.7,
+                    "&:hover": {
+                      opacity: 1
+                    },
+                  })}
+                  to={option.link}
+                  selected={i === selectedIndex && value === 1}
+                >
+                  {option.name}
+                </MenuItem>))}
             </Menu>
           </Toolbar>
         </AppBar>
--- src/components/ui/Theme.js
+++ src/components/ui/Theme.js
@@ -31,7 +31,7 @@ const theme = createTheme({
       textTransform: "none",
       color: "white",
     } 
-  }
+  },
 });
 
 export default theme
